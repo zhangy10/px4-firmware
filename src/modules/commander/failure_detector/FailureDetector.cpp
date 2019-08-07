622bc94e91b6723400b9fc24113e5c337a875c86
bresch <brescianimathieu@gmail.com>
2019-08-07 11:35:05 +0200

    FailureDetector - Ignore attitude check for MC in acro and FW in manual, acro and rattitude modes

--- src/modules/commander/failure_detector/FailureDetector.cpp
+++ src/modules/commander/failure_detector/FailureDetector.cpp
@@ -43,8 +43,20 @@
 FailureDetector::FailureDetector(ModuleParams *parent) :
 	ModuleParams(parent),
 	_sub_vehicle_attitude_setpoint(ORB_ID(vehicle_attitude_setpoint)),
-	_sub_vehicule_attitude(ORB_ID(vehicle_attitude))
+	_sub_vehicule_attitude(ORB_ID(vehicle_attitude)),
+	_sub_vehicle_status(ORB_ID(vehicle_status))
 {
+}
+
+bool FailureDetector::resetStatus()
+{
+	if (_status != FAILURE_NONE) {
+		_status = FAILURE_NONE;
+		return true;
+
+	} else {
+		return false;
+	}
 }
 
 bool
@@ -52,9 +64,46 @@ FailureDetector::update()
 {
 	bool updated(false);
 
-	updated = updateAttitudeStatus();
+	if (isAttitudeStabilized()) {
+		updated = updateAttitudeStatus();
+
+	} else {
+		updated = resetStatus();
+	}
 
 	return updated;
+}
+
+bool
+FailureDetector::isAttitudeStabilized()
+{
+
+	if (_sub_vehicle_status.update()) {
+		const vehicle_status_s &vehicle_status = _sub_vehicle_status.get();
+		const uint8_t vehicle_type = vehicle_status.vehicle_type;
+		const uint8_t nav_state = vehicle_status.nav_state;
+
+		if (vehicle_type == vehicle_status_s::VEHICLE_TYPE_ROTARY_WING) {
+			if (nav_state == vehicle_status_s::NAVIGATION_STATE_ACRO) {
+				_attitude_is_stabilized = false;
+
+			} else {
+				_attitude_is_stabilized = true;
+			}
+
+		} else if (vehicle_type == vehicle_status_s::VEHICLE_TYPE_FIXED_WING) {
+			if (nav_state == vehicle_status_s::NAVIGATION_STATE_MANUAL ||
+			    nav_state == vehicle_status_s::NAVIGATION_STATE_ACRO ||
+			    nav_state == vehicle_status_s::NAVIGATION_STATE_RATTITUDE) {
+				_attitude_is_stabilized = false;
+
+			} else {
+				_attitude_is_stabilized = true;
+			}
+		}
+	}
+
+	return _attitude_is_stabilized;
 }
 
 bool
