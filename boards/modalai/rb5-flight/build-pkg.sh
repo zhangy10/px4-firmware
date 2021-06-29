c1d5704dc4bc0bd8d61acff26032fffac6336aca
Eric Katzfey <eric.katzfey@modalai.com>
2021-06-29 09:38:59 -0700

    Added parameter directory creation and mixer file installation to the package postinst script

--- boards/modalai/rb5-flight/build-pkg.sh
+++ boards/modalai/rb5-flight/build-pkg.sh
@@ -6,6 +6,11 @@ rm -fR debian/usr
 mkdir -p debian/usr/lib/rfsa/adsp
 cp ../../../build/modalai_rb5-flight_qurt/platforms/qurt/libpx4.so debian/usr/lib/rfsa/adsp
 
+# Install quadrotor mixer file for ESC.
+# The mixer file comes from ROMFS/px4fmu_common/mixers
+adb push quad_x.main.mix /usr/lib/rfsa/adsp
+cp ../../../ROMFS/px4fmu_common/mixers/quad_x.main.mix debian/usr/lib/rfsa/adsp
+
 mkdir -p debian/usr/bin
 cp ../../../build/modalai_rb5-flight_default/bin/px4 debian/usr/bin
 cp ../../../build/modalai_rb5-flight_default/bin/px4-alias.sh debian/usr/bin
@@ -20,5 +25,8 @@ cp full-m0051.config debian/etc/modalai
 mkdir -p debian/etc/systemd/system
 cp net-check.service debian/etc/systemd/system
 cp px4-start.service debian/etc/systemd/system
+
+# Create necessary directories for px4 operation
+mkdir -p debian/home/linaro/eeprom
 
 dpkg-deb --build debian pkg
