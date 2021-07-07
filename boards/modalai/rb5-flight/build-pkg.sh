196c33efb2db837d25fd0f89392f388108a3d70b
Eric Katzfey <eric.katzfey@modalai.com>
2021-07-06 18:14:24 -0700

    Version 1.0.2

--- boards/modalai/rb5-flight/build-pkg.sh
+++ boards/modalai/rb5-flight/build-pkg.sh
@@ -8,13 +8,13 @@ cp ../../../build/modalai_rb5-flight_qurt/platforms/qurt/libpx4.so debian/usr/li
 
 # Install quadrotor mixer file for ESC.
 # The mixer file comes from ROMFS/px4fmu_common/mixers
-adb push quad_x.main.mix /usr/lib/rfsa/adsp
 cp ../../../ROMFS/px4fmu_common/mixers/quad_x.main.mix debian/usr/lib/rfsa/adsp
 
 mkdir -p debian/usr/bin
 cp ../../../build/modalai_rb5-flight_default/bin/px4 debian/usr/bin
 cp ../../../build/modalai_rb5-flight_default/bin/px4-alias.sh debian/usr/bin
 cp net-check.sh debian/usr/bin
+chmod a+x debian/usr/bin/net-check.sh
 chmod a+x debian/usr/bin/px4-alias.sh
 
 rm -fR debian/etc
