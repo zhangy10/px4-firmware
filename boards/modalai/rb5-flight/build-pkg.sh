41e28bac283996aad02c24230d8fb10c5a55ba79
Eric Katzfey <eric.katzfey@modalai.com>
2021-08-05 15:11:26 -0700

    Changed the way parameters are handled such that any parameter can be overwritten if needed. Also did some cleanup of the scripts

--- boards/modalai/rb5-flight/build-pkg.sh
+++ boards/modalai/rb5-flight/build-pkg.sh
@@ -13,18 +13,15 @@ cp ../../../ROMFS/px4fmu_common/mixers/quad_x.main.mix debian/usr/lib/rfsa/adsp
 mkdir -p debian/usr/bin
 cp ../../../build/modalai_rb5-flight_default/bin/px4 debian/usr/bin
 cp ../../../build/modalai_rb5-flight_default/bin/px4-alias.sh debian/usr/bin
-cp net-check.sh debian/usr/bin
-chmod a+x debian/usr/bin/net-check.sh
 chmod a+x debian/usr/bin/px4-alias.sh
+cp m0052-px4 debian/usr/bin
+chmod a+x debian/usr/bin/m0052-px4
 
 rm -fR debian/etc
 mkdir -p debian/etc/modalai
-cp min-m0051.config debian/etc/modalai
-cp full-m0051.config debian/etc/modalai
-
-mkdir -p debian/etc/systemd/system
-cp net-check.service debian/etc/systemd/system
-cp px4-start.service debian/etc/systemd/system
+cp min-m0052.config debian/etc/modalai
+cp full-m0052.config debian/etc/modalai
+cp m0052-set-default-parameters.config debian/etc/modalai
 
 # Create necessary directories for px4 operation
 mkdir -p debian/home/linaro/eeprom
--- boards/modalai/rb5-flight/install-rb5-qurt.sh
+++ boards/modalai/rb5-flight/install-rb5-qurt.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
 adb push ../../../build/modalai_rb5-flight_qurt/platforms/qurt/libpx4.so /usr/lib/rfsa/adsp
-# adb push ../../../build/modalai_rb5-flight_qurt/platforms/qurt/libpx4muorb_skel.so /usr/lib/rfsa/adsp
 
 adb shell sync
