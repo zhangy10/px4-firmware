3902100012cd31d56c4e05b45f1be235ca6da6c4
Eric Katzfey <eric.katzfey@modalai.com>
2021-08-29 11:55:02 -0700

    Updated the IMU server to deal with the PX4 IMU driver

--- src/drivers/modalai/imu_server/imu_server.cpp
+++ src/drivers/modalai/imu_server/imu_server.cpp
@@ -62,6 +62,15 @@ static int _imu_server_thread(int argc, char *argv[]) {
     struct sensor_accel_s accel_data;
     struct sensor_gyro_s  gyro_data;
     icm4x6xx_imu_data_t   imu_data;
+    uint64_t              previous_timestamp = 0;
+
+    memset(&imu_data, 0, sizeof(imu_data));
+
+    // We send out the data when the accel timestamp is the same as the
+    // gyro timestamp. That means they are a matched set and can be combined
+    // into the imu data packet. Start with them unequal.
+    accel_data.timestamp_sample = 0;
+    gyro_data.timestamp_sample = 1;
 
     px4_pollfd_struct_t fds[2] = { { .fd = sensor_accel_fd,  .events = POLLIN },
                                    { .fd = sensor_gyro_fd, .events = POLLIN } };
@@ -75,32 +84,39 @@ static int _imu_server_thread(int argc, char *argv[]) {
             // PX4_INFO("Got accel data %lu", accel_data.timestamp_sample);
     	} else if (fds[1].revents & POLLIN) {
 		    orb_copy(ORB_ID(sensor_gyro), sensor_gyro_fd, &gyro_data);
-            imu_data.gyro_rad[0]            = gyro_data.x;
-            imu_data.gyro_rad[1]            = gyro_data.y;
-            imu_data.gyro_rad[2]            = gyro_data.z;
+            imu_data.gyro_rad[0] = gyro_data.x;
+            imu_data.gyro_rad[1] = gyro_data.y;
+            imu_data.gyro_rad[2] = gyro_data.z;
+            // PX4_INFO("Got gyro data %lu", gyro_data.timestamp_sample);
+        }
 
-            // The gyro data always comes in right after the accel data so use
-            // it as a trigger to send out the full report
+        if (accel_data.timestamp_sample == gyro_data.timestamp_sample) {
+
             imu_data.temp_c                 = gyro_data.temperature;
             imu_data.timestamp_monotonic_ns = gyro_data.timestamp_sample * 1000;
 
-            // Write the data to the fifo
-            size_t data_len = sizeof(icm4x6xx_imu_data_t);
-            ssize_t bytes_written = write(fifo_fd, &imu_data, data_len);
-            size_t unsigned_bytes_written = (bytes_written > 0) ? (size_t) bytes_written : 0;
-            if ((bytes_written > 0) && (unsigned_bytes_written == data_len)) {
-                // PX4_INFO("Wrote %ld IMU data bytes to %s", unsigned_bytes_written, imu_fifo);
+            if (previous_timestamp < imu_data.timestamp_monotonic_ns) {
+                // Write the data to the fifo
+                size_t data_len = sizeof(icm4x6xx_imu_data_t);
+                ssize_t bytes_written = write(fifo_fd, &imu_data, data_len);
+                size_t unsigned_bytes_written = (bytes_written > 0) ? (size_t) bytes_written : 0;
+                if ((bytes_written > 0) && (unsigned_bytes_written == data_len)) {
+                    // PX4_INFO("Wrote %ld IMU data bytes to %s", unsigned_bytes_written, imu_fifo);
+                } else {
+                    PX4_ERR("Error: Couldn't write %lu bytes to the pipe", data_len);
+                    PX4_ERR("       write returned %ld", bytes_written);
+                    break;
+                }
             } else {
-                PX4_ERR("Error: Couldn't write %lu bytes to the pipe", data_len);
-                PX4_ERR("       write returned %ld", bytes_written);
-                break;
+                PX4_WARN("*** Dropping stale IMU data. Previous %lu, current %lu ***", previous_timestamp, imu_data.timestamp_monotonic_ns);
             }
+            previous_timestamp = imu_data.timestamp_monotonic_ns;
 
-            // PX4_INFO("%.2f %.2f %.2f %.2f %.2f %.2f %.2f %lu",
+            // PX4_INFO("**** %.2f %.2f %.2f %.2f %.2f %.2f %.2f %lu ****",
             //          (double) imu_data.accl_ms2[0], (double) imu_data.accl_ms2[1],
             //          (double) imu_data.accl_ms2[2], (double) imu_data.gyro_rad[0],
             //          (double) imu_data.gyro_rad[1], (double) imu_data.gyro_rad[2],
-            //          (double) imu_data.temp_c, (imu_data.timestamp_monotonic_ns / 1000));
+            //          (double) imu_data.temp_c, gyro_data.timestamp_sample);
         }
     }
 
