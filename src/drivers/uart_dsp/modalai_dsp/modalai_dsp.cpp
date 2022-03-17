018f06483c90dfa653bffbca6fcd18b9e10705bb
Your Name <you@example.com>
2022-03-17 09:25:48 -0500

    adding mavlink hitl receiver functions into code base

--- src/drivers/uart_dsp/modalai_dsp/modalai_dsp.cpp
+++ src/drivers/uart_dsp/modalai_dsp/modalai_dsp.cpp
@@ -41,6 +41,8 @@
 #include <drivers/device/qurt/uart.h>
 #endif
 
+#include <v2.0/standard/mavlink.h>
+
 #include <uORB/uORB.h>
 #include <uORB/topics/sensor_gps.h>
 #include <uORB/topics/battery_status.h>
@@ -76,10 +78,30 @@ extern "C" { __EXPORT int modalai_dsp_main(int argc, char *argv[]); }
 namespace modalai_dsp
 {
 
+using matrix::wrap_2pi;
+
 static bool _is_running = false;
 volatile bool _task_should_exit = false;
 static px4_task_t _task_handle = -1;
 int _uart_fd = -1;
+
+uORB::Publication<battery_status_s>			_battery_pub{ORB_ID(battery_status)};
+uORB::Publication<sensor_gps_s>				_gps_pub{ORB_ID(sensor_gps)};
+uORB::Publication<differential_pressure_s>		_differential_pressure_pub{ORB_ID(differential_pressure)};
+
+// hil_sensor and hil_state_quaternion
+enum SensorSource {
+	ACCEL		= 0b111,
+	GYRO		= 0b111000,
+	MAG		= 0b111000000,
+	BARO		= 0b1101000000000,
+	DIFF_PRESS	= 0b10000000000
+};
+
+PX4Accelerometer *_px4_accel{nullptr};
+PX4Barometer *_px4_baro{nullptr};
+PX4Gyroscope *_px4_gyro{nullptr};
+PX4Magnetometer *_px4_mag{nullptr};
 
 int openPort(const char *dev, speed_t speed);
 int closePort();
@@ -293,8 +315,146 @@ usage()
 	PX4_INFO("Usage: modalai_dsp {start|info|stop}");
 }
 
+void
+handle_message_hil_sensor_dsp(mavlink_message_t *msg)
+{
+	mavlink_hil_sensor_t hil_sensor;
+	mavlink_msg_hil_sensor_decode(msg, &hil_sensor);
+
+	const uint64_t timestamp = hrt_absolute_time();
+
+	// temperature only updated with baro
+	float temperature = NAN;
+
+	if ((hil_sensor.fields_updated & SensorSource::BARO) == SensorSource::BARO) {
+		temperature = hil_sensor.temperature;
+	}
+
+	// gyro
+	if ((hil_sensor.fields_updated & SensorSource::GYRO) == SensorSource::GYRO) {
+		if (_px4_gyro == nullptr) {
+			// 1310988: DRV_IMU_DEVTYPE_SIM, BUS: 1, ADDR: 1, TYPE: SIMULATION
+			_px4_gyro = new PX4Gyroscope(1310988);
+		}
+
+		if (_px4_gyro != nullptr) {
+			if (PX4_ISFINITE(temperature)) {
+				_px4_gyro->set_temperature(temperature);
+			}
+
+			_px4_gyro->update(timestamp, hil_sensor.xgyro, hil_sensor.ygyro, hil_sensor.zgyro);
+		}
+	}
+
+	// accelerometer
+	if ((hil_sensor.fields_updated & SensorSource::ACCEL) == SensorSource::ACCEL) {
+		if (_px4_accel == nullptr) {
+			// 1310988: DRV_IMU_DEVTYPE_SIM, BUS: 1, ADDR: 1, TYPE: SIMULATION
+			_px4_accel = new PX4Accelerometer(1310988);
+		}
+
+		if (_px4_accel != nullptr) {
+			if (PX4_ISFINITE(temperature)) {
+				_px4_accel->set_temperature(temperature);
+			}
+
+			_px4_accel->update(timestamp, hil_sensor.xacc, hil_sensor.yacc, hil_sensor.zacc);
+		}
+	}
+
+	// magnetometer
+	if ((hil_sensor.fields_updated & SensorSource::MAG) == SensorSource::MAG) {
+		if (_px4_mag == nullptr) {
+			// 197388: DRV_MAG_DEVTYPE_MAGSIM, BUS: 3, ADDR: 1, TYPE: SIMULATION
+			_px4_mag = new PX4Magnetometer(197388);
+		}
+
+		if (_px4_mag != nullptr) {
+			if (PX4_ISFINITE(temperature)) {
+				_px4_mag->set_temperature(temperature);
+			}
+
+			_px4_mag->update(timestamp, hil_sensor.xmag, hil_sensor.ymag, hil_sensor.zmag);
+		}
+	}
+
+	// baro
+	if ((hil_sensor.fields_updated & SensorSource::BARO) == SensorSource::BARO) {
+		if (_px4_baro == nullptr) {
+			// 6620172: DRV_BARO_DEVTYPE_BAROSIM, BUS: 1, ADDR: 4, TYPE: SIMULATION
+			_px4_baro = new PX4Barometer(6620172);
+		}
+
+		if (_px4_baro != nullptr) {
+			_px4_baro->set_temperature(hil_sensor.temperature);
+			_px4_baro->update(timestamp, hil_sensor.abs_pressure);
+		}
+	}
+
+	// differential pressure
+	if ((hil_sensor.fields_updated & SensorSource::DIFF_PRESS) == SensorSource::DIFF_PRESS) {
+		differential_pressure_s report{};
+		report.timestamp = timestamp;
+		report.temperature = hil_sensor.temperature;
+		report.differential_pressure_filtered_pa = hil_sensor.diff_pressure * 100.0f; // convert from millibar to bar;
+		report.differential_pressure_raw_pa = hil_sensor.diff_pressure * 100.0f; // convert from millibar to bar;
+
+		_differential_pressure_pub.publish(report);
+	}
+
+	// battery status
+	{
+		battery_status_s hil_battery_status{};
+
+		hil_battery_status.timestamp = timestamp;
+		hil_battery_status.voltage_v = 11.5f;
+		hil_battery_status.voltage_filtered_v = 11.5f;
+		hil_battery_status.current_a = 10.0f;
+		hil_battery_status.discharged_mah = -1.0f;
+
+		_battery_pub.publish(hil_battery_status);
+	}
 }
 
+void
+handle_message_hil_gps_dsp(mavlink_message_t *msg)
+{
+	mavlink_hil_gps_t gps;
+	mavlink_msg_hil_gps_decode(msg, &gps);
+
+	const uint64_t timestamp = hrt_absolute_time();
+
+	sensor_gps_s hil_gps{};
+
+	hil_gps.timestamp_time_relative = 0;
+	hil_gps.time_utc_usec = gps.time_usec;
+
+	hil_gps.timestamp = timestamp;
+	hil_gps.lat = gps.lat;
+	hil_gps.lon = gps.lon;
+	hil_gps.alt = gps.alt;
+	hil_gps.eph = (float)gps.eph * 1e-2f; // from cm to m
+	hil_gps.epv = (float)gps.epv * 1e-2f; // from cm to m
+
+	hil_gps.s_variance_m_s = 0.1f;
+
+	hil_gps.vel_m_s = (float)gps.vel * 1e-2f; // from cm/s to m/s
+	hil_gps.vel_n_m_s = gps.vn * 1e-2f; // from cm to m
+	hil_gps.vel_e_m_s = gps.ve * 1e-2f; // from cm to m
+	hil_gps.vel_d_m_s = gps.vd * 1e-2f; // from cm to m
+	hil_gps.vel_ned_valid = true;
+	hil_gps.cog_rad = ((gps.cog == 65535) ? NAN : wrap_2pi(math::radians(gps.cog * 1e-2f)));
+
+	hil_gps.fix_type = gps.fix_type;
+	hil_gps.satellites_used = gps.satellites_visible;  //TODO: rename mavlink_hil_gps_t sats visible to used?
+
+	hil_gps.heading = NAN;
+	hil_gps.heading_offset = NAN;
+
+	_gps_pub.publish(hil_gps);
+}
+
+}
 int modalai_dsp_main(int argc, char *argv[])
 {
 	int myoptind = 1;
