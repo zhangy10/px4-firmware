ff0538630b9a244e4438868cfd7b35aaf2125ccb
Your Name <you@example.com>
2022-04-08 16:02:30 -0500

    test to implement 0s for VIO uorb pub

--- src/drivers/uart_dsp/modalai_dsp/modalai_dsp.cpp
+++ src/drivers/uart_dsp/modalai_dsp/modalai_dsp.cpp
@@ -64,6 +64,7 @@
 #include <uORB/topics/vehicle_control_mode.h>
 #include <uORB/topics/vehicle_status.h>
 #include <uORB/topics/telemetry_status.h>
+#include <uORB/topics/vehicle_odometry.h>
 
 #include <lib/drivers/accelerometer/PX4Accelerometer.hpp>
 #include <lib/drivers/barometer/PX4Barometer.hpp>
@@ -102,6 +103,9 @@ int _uart_fd = -1;
 uORB::Publication<battery_status_s>			_battery_pub{ORB_ID(battery_status)};
 uORB::Publication<sensor_gps_s>				_gps_pub{ORB_ID(sensor_gps)};
 uORB::Publication<differential_pressure_s>		_differential_pressure_pub{ORB_ID(differential_pressure)};
+//NEW ODOMETRY CODE
+uORB::Publication<vehicle_odometry_s>			_visual_odometry_pub{ORB_ID(vehicle_visual_odometry)};
+//END ODOMETRY CODE
 
 // hil_sensor and hil_state_quaternion
 enum SensorSource {
@@ -168,6 +172,10 @@ void handle_message_heartbeat_dsp(mavlink_message_t *msg);
 void CheckHeartbeats(const hrt_abstime &t, bool force);
 void handle_message_dsp(mavlink_message_t *msg);
 void actuator_controls_from_outputs_dsp(mavlink_hil_actuator_controls_t *msg);
+
+//NEW ODOMETRY CODE
+void handle_message_odometry_dsp();
+//END NEW ODOMETRY CODE
 
 void
 handle_message_dsp(mavlink_message_t *msg)
@@ -267,6 +275,9 @@ void task_main(int argc, char *argv[])
 				}
 			}
 		}
+		//NEW ODOMETRY CODE
+		handle_message_odometry_dsp();
+		//END NEW ODOMETRY CODE
 
 		bool vehicle_updated = false;
         (void) orb_check(_vehicle_status_sub, &vehicle_updated);
@@ -321,6 +332,99 @@ void task_main(int argc, char *argv[])
 		// if (elapsed_time < 10000) usleep(10000 - elapsed_time);
 		if (elapsed_time < 5000) usleep(5000 - elapsed_time);
 	}
+}
+
+void
+handle_message_odometry_dsp()
+{
+	//mavlink_odometry_t odom;
+	//mavlink_msg_odometry_decode(msg, &odom);
+
+	vehicle_odometry_s odometry{};
+
+	odometry.timestamp = hrt_absolute_time();
+	//odometry.timestamp_sample = _mavlink_timesync.sync_stamp(odom.time_usec);
+	odometry.timestamp_sample = hrt_absolute_time();
+
+	/* The position is in a local FRD frame */
+	//odometry.x = odom.x;
+	//odometry.y = odom.y;
+	//odometry.z = odom.z;
+	odometry.x = 0;
+	odometry.y = 0;
+	odometry.z = 0;
+	/**
+	 * The quaternion of the ODOMETRY msg represents a rotation from body frame
+	 * to a local frame
+	 */
+	// matrix::Quatf q_body_to_local(odom.q);
+	// q_body_to_local.normalize();
+	// q_body_to_local.copyTo(odometry.q);
+
+	// // pose_covariance
+	static constexpr size_t POS_URT_SIZE = sizeof(odometry.pose_covariance) / sizeof(odometry.pose_covariance[0]);
+	// static_assert(POS_URT_SIZE == (sizeof(odom.pose_covariance) / sizeof(odom.pose_covariance[0])),
+	// 	      "Odometry Pose Covariance matrix URT array size mismatch");
+
+	// // velocity_covariance
+	static constexpr size_t VEL_URT_SIZE = sizeof(odometry.velocity_covariance) / sizeof(odometry.velocity_covariance[0]);
+	// static_assert(VEL_URT_SIZE == (sizeof(odom.velocity_covariance) / sizeof(odom.velocity_covariance[0])),
+	// 	      "Odometry Velocity Covariance matrix URT array size mismatch");
+
+	// TODO: create a method to simplify covariance copy
+	for (size_t i = 0; i < POS_URT_SIZE; i++) {
+		//odometry.pose_covariance[i] = odom.pose_covariance[i];
+		odometry.pose_covariance[i] = 0;
+	}
+
+	/**
+	 * PX4 expects the body's linear velocity in the local frame,
+	 * the linear velocity is rotated from the odom child_frame to the
+	 * local NED frame. The angular velocity needs to be expressed in the
+	 * body (fcu_frd) frame.
+	 */
+	//if (odom.child_frame_id == MAV_FRAME_BODY_FRD) {
+
+	odometry.velocity_frame = vehicle_odometry_s::BODY_FRAME_FRD;
+	odometry.vx = 0;
+	odometry.vy = 0;
+	odometry.vz = 0;
+
+	odometry.rollspeed = 0;
+	odometry.pitchspeed = 0;
+	odometry.yawspeed = 0;
+
+	for (size_t i = 0; i < VEL_URT_SIZE; i++) {
+		odometry.velocity_covariance[i] = 0;
+	}
+
+	//} else {
+	//	PX4_ERR("Body frame %u not supported. Unable to publish velocity", odom.child_frame_id);
+	//}
+
+	/**
+	 * Supported local frame of reference is MAV_FRAME_LOCAL_NED or MAV_FRAME_LOCAL_FRD
+	 * The supported sources of the data/tesimator type are MAV_ESTIMATOR_TYPE_VISION,
+	 * MAV_ESTIMATOR_TYPE_VIO and MAV_ESTIMATOR_TYPE_MOCAP
+	 *
+	 * @note Regarding the local frames of reference, the appropriate EKF_AID_MASK
+	 * should be set in order to match a frame aligned (NED) or not aligned (FRD)
+	 * with true North
+	 */
+	//if (odom.frame_id == MAV_FRAME_LOCAL_NED || odom.frame_id == MAV_FRAME_LOCAL_FRD) {
+
+	//	if (odom.frame_id == MAV_FRAME_LOCAL_NED) {
+	//odometry.local_frame = vehicle_odometry_s::LOCAL_FRAME_NED;
+
+	//	} else {
+	odometry.local_frame = vehicle_odometry_s::LOCAL_FRAME_FRD;
+		//}
+
+	_visual_odometry_pub.publish(odometry);
+
+	//} else {
+	//	PX4_ERR("Local frame %u not supported. Unable to publish pose and velocity", odom.frame_id);
+	//}
 }
 
 void actuator_controls_from_outputs_dsp(mavlink_hil_actuator_controls_t *msg)
